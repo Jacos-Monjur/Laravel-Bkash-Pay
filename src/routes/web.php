02b1e0dc0cd9f3209f3e4f4fdfd7370180cf8049
Monjur <jacosmonjurajad@gmail.com>
2022-09-17 15:59:09 +0600

    update routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => 'monjur\bkashpay\Http\Controllers'], function () {
+Route::group(['namespace' => 'monjur\bkash\Http\Controllers'], function () {
     Route::get('demo-order', 'PaymentController@index')->name('order');
     Route::post('token', 'PaymentController@token')->name('token');
     Route::get('createpayment', 'PaymentController@createpayment')->name('createpayment');
