81661df91aa165ed13b86f19f376dfeba20f1675
Monjur <jacosmonjurajad@gmail.com>
2022-09-18 12:03:53 +0600

    updated

--- src/Http/Controllers/BkashPayController.php
+++ src/Http/Controllers/BkashPayController.php
@@ -32,6 +32,7 @@ class BkashPayController extends Controller
         return view('bkash::Bkash.load', compact('order'));
     }
 
+
     public function token()
     {
         session_start();
@@ -41,15 +42,15 @@ class BkashPayController extends Controller
 
         $_SESSION['token'] = $idtoken;
 
-        // $strJsonFileContents = file_get_contents($this->bkashJson());
-        // $array = json_decode($strJsonFileContents, true);
+        /*$strJsonFileContents = file_get_contents("config.json");
+        $array = json_decode($strJsonFileContents, true);*/
 
         $array = $this->get_config_file();
 
         $array['token'] = $idtoken;
 
         $newJsonString = json_encode($array);
-        File::put(storage_path('/app/public/bkash.json'), $newJsonString);
+        File::put(storage_path() . '/app/public/config.json', $newJsonString);
 
         echo $idtoken;
     }
@@ -89,8 +90,7 @@ class BkashPayController extends Controller
 
     protected function get_config_file()
     {
-        $path = resource_path('/views/Bkash/bkash.json');
-        // $path = $this->bkashJson();
+        $path = resource_path() . "/views/Bkash/bkash.json";
         return json_decode(file_get_contents($path), true);
     }
 
@@ -159,12 +159,12 @@ class BkashPayController extends Controller
         $resultdatax = curl_exec($url);
         curl_close($url);
 
-        $this->updateOrder($resultdatax);
+        $this->_updateOrderStatus($resultdatax);
 
         echo $resultdatax;
     }
 
-    protected function updateOrder($resultdatax)
+    protected function _updateOrderStatus($resultdatax)
     {
         $resultdatax = json_decode($resultdatax);
         // dd($resultdatax);
@@ -175,23 +175,5 @@ class BkashPayController extends Controller
                 'status' => 'Processing', 'trxID' => $resultdatax->trxID
             ]);
         }
-    }
-
-    protected function bkashJson()
-    {
-        $json = '{
-            "createURL": "https://checkout.sandbox.bka.sh/v1.2.0-beta/checkout/payment/create",
-            "executeURL": "https://checkout.sandbox.bka.sh/v1.2.0-beta/checkout/payment/execute/",
-            "tokenURL": "https://checkout.sandbox.bka.sh/v1.2.0-beta/checkout/token/grant",
-            "script": "https://scripts.sandbox.bka.sh/versions/1.2.0-beta/checkout/bKash-checkout-sandbox.js",
-            "app_key": "5tunt4masn6pv2hnvte1sb5n3j",
-            "proxy": "",
-            "app_secret": "1vggbqd4hqk9g96o9rrrp2jftvek578v7d2bnerim12a87dbrrka",
-            "username": "sandboxTestUser",
-            "password": "hWD@8vtzw0",
-            "token": "eyJraWQiOiJmalhJQmwxclFUXC9hM215MG9ScXpEdVZZWk5KXC9qRTNJOFBaeGZUY3hlamc9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJiM2Q4OGVkZC0xNzc2LTRhMjEtYWZlMi0zN2FkZTk3NzEyZDMiLCJhdWQiOiI2NmEwdGZpYTZvc2tkYjRhMDRyY24wNjNhOSIsImV2ZW50X2lkIjoiYjdiYWM1MTMtOTY2MS00YWI1LWFmYzMtZmE2OGM3OWM4ZmRmIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE2NjI2NTMyNDAsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1zb3V0aGVhc3QtMS5hbWF6b25hd3MuY29tXC9hcC1zb3V0aGVhc3QtMV9rZjVCU05vUGUiLCJjb2duaXRvOnVzZXJuYW1lIjoic2FuZGJveFRlc3RVc2VyIiwiZXhwIjoxNjYyNjU2ODQwLCJpYXQiOjE2NjI2NTMyNDB9.F4ACD2SEnFx07Q5Xpq9ONpCruCg9Sr8RPWuM4xOwNnJIJfgeROf6KdzqFbsTilj-MY9gstR_I2ocR1EKaTAPNOshxSxmIIvJr5S5DIwocSNWrA6eGHHe1qh1H7-I7lxmiblOya8xOY7o8Mb-dj4r5Yw8EV_ZByxO-6r_GjupFjlXv0Uag-_ISU6LeFuB4Iql-nIX_cKCtg9kiHcgNT3bVVsmukik5dkeDsBmXbFgJb8eMBFXaKPxNzvJftJbHUuab9qppefEUa1gE3Fvif9QLMEQt0x7E98ZP96ZYmT9s_t-SOnuSY3i1qR6IxPXE27_4Oz10I5-FQV-hottuKHMpQ"
-        }';
-
-        return json_decode($json);
     }
 }
